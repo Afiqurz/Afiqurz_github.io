63392ec45dbd745d6d7fd5a251e1ef80f23176dd
Afiqurz <121597410+Afiqurz@users.noreply.github.com>
2024-04-17 00:10:04 +0700

    Add files via upload

---
+++ A.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba coba</title>
+</head>
+<body>
+<h1>hi iam seva</h1>
+<P>Saya beralamat dirumah  manyar </P>
+<p>this is a another paragraf</p>
+<a href="https://www.w3school.com">tekan link ini</a>
+<p title="John 'jhon Shotgun'"></p>
+<h1>Aku sangat menyukai hal ini</h1>
+<P>dan ini adalah tentang kisah Aku</P>
+<hr>
+<h>ini adalah otaku </h>
+<p>ini  adalah text yang kubuat</p>
+<hr>
+<pre>
+  aku adalah seorang pengusaha
+  aku orang yang bekerja keras
+  <br>
+  dan aku adalah manusia biasa
+</pre>
+<p>hari ini aku sangat suka mengetik</p>
+<img src="w3chool.jpg" alt="w3chool.com" width="50" height="100">
+<form action="">
+  <table border="1">
+      <tr>
+        <td colspan="3" align="center"> </td>
+        
+      </tr>
+  </table>
+  
+</form>
+</body>
+</html>
